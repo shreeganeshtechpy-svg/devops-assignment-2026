ad95448a0446139e94f20c91d4658381bcab2f64
shreeganeshtechpy-svg <shreeganesh.tech.py@gmail.com>
2026-03-14 15:28:40 +0530

    update readme and dashboard UI

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,7 +13,7 @@ variable "project_name" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.micro"
+  default     = "t3.micro"
 }
 
 variable "ami_id" {
@@ -25,5 +25,5 @@ variable "ami_id" {
 variable "public_key" {
   description = "SSH public key for EC2 key pair"
   type        = string
-  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC0 placeholder-replace-with-real-key"
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCvLRIoGO9FDmtJ5ycGnAnWZf5ic1+9K4VALWEne2jc9EnA2ORfoQnlQeRwsx1A1JOKDrymefmUk3qlxQ1C0clvzu4en+jQ9YPZLx/emrZYPGcvsdy/nk7+RlAqee1b+t+23HlYOXrRoRzRTinxqb0b4ZpZ/1zybtoD6TUhCFR5Jl7chLpicAv52t7snt5wP5PXLJVBYqp4hlUXi9RQxnypKllhcimr8IaB38sdo0lTHk/Xtt30NkNR7hZ6pjEJSS6K1+Z+evR3AeIuPzXC2UvXmpl9ZJbM8dgk0HCsF0EiNuBcxeWzKxnF4vg36q6AT4iJJX4YwvXSw76fS29kFqBD vrajm@LAPTOP-S0DVK14V"
 }
